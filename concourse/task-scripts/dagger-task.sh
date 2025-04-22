233a1954f9414a5342cefd1fdefba7f002b2a243
sezginerdem <deployment@chillibean.com>
2025-04-22 17:26:44 +0100

    change path

--- concourse/task-scripts/dagger-task.sh
+++ concourse/task-scripts/dagger-task.sh
@@ -8,6 +8,12 @@ export PATH="$HOME/.dagger/bin:$PATH"
 export DAGGER_CLI_BIN="$HOME/.dagger/bin/dagger"
 export DAGGER_ENGINE=houdini
 
-echo "🚀 Running Go + Dagger"
 cd ruby-hello-dagger/dagger
+
+echo "📦 Initializing Go module"
+go mod init example.com/dagger-task || true
+go get dagger.io/dagger@v0.8.7
+go mod tidy
+
+echo "🚀 Running Go + Dagger"
 go run main.go
