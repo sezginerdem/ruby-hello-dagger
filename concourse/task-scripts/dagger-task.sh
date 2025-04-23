6aa00e6b330d0ca64f95597dd1bb4e15990946bf
sezginerdem <deployment@chillibean.com>
2025-04-23 05:30:26 +0100

    change path

--- concourse/task-scripts/dagger-task.sh
+++ concourse/task-scripts/dagger-task.sh
@@ -2,15 +2,18 @@
 set -e
 
 echo "🔧 Installing Dagger CLI..."
+mkdir -p ./bin
 curl -L https://dl.dagger.io/dagger/install.sh | DAGGER_VERSION=0.8.7 sh
 
-export PATH="$HOME/.dagger/bin:$PATH"
+export PATH="$(pwd)/bin:$PATH"
 export DAGGER_ENGINE=houdini
 
-echo "📦 Initializing Go module"
 echo "📂 Current directory: $(pwd)"
 ls -la
+
 cd ruby-hello-dagger/dagger
+
+echo "📦 Initializing Go module"
 go mod init example.com/dagger-task || true
 go get dagger.io/dagger@v0.8.7
 go mod tidy
